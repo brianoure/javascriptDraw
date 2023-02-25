951edd825f6b98e98e07e7906831bc273064d2d6
brianoure <61065227+brianoure@users.noreply.github.com>
2023-02-25 18:27:30 +0300

    Update example1.js

--- example1.js
+++ example1.js
@@ -1,19 +1,38 @@
+//<script>
 
-<script>
+var drawingheight=100;
+var drawingwidth=100;
+var dh=drawingheight;
+var dw=drawingwidth;
+var color_raster=[];
+//to access a pixel(pixel_row_number*100)+pixel_col_number.....PS...only works with strings//
+
+
 function doc(x){document.write(x);}
 function docId(id){return document.getElementById(id);}
-function color_id(id,clr){docId(id).style.backgroundColor=clr;}
+function color_id(id,clr){docId(id).style.background=clr;}
+
+
+//initialize
+for(let pixel=0;pixel<(drawingheight*drawingwidth);pixel++){//for
+    //doc("pixel");
+    color_raster.push("rgb(0,0,0)");// only works with strings
+}//for
+
 
 doc("<table id='mycanvas'>");
-for(let r=0;r<100;r++){//for
+for(let r=0;r<drawingheight;r++){//for
    doc("<tr>");
-   for(let c=0;c<100;c++){//for
+   for(let c=0;c<drawingwidth;c++){//for
        let idstring = "r"+r+"c"+c;
        doc("<td id='"+idstring+"'></td>");
-       color_id(idstring,"blue");
+       color_id(idstring,color_raster[  (dh*r)+c  ]);
    }//for
    doc("</tr>");
 }//for
 doc("</table>");
 docId("mycanvas").style.borderCollapse = "collapse";//eliminate spaces between pixels
-</script>
+
+//</script>
+
+
